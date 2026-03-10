5535ab8cd95104637f015006d3e2f358d7970e93
Stephen <stephenn649@gmail.com>
2026-03-10 14:45:25 -0500

    Adds Paid Holidays view for the user in Timesheet page

--- src/pages/timeSheet/controls/Holidays.tsx
+++ src/pages/timeSheet/controls/Holidays.tsx
@@ -1,7 +1,59 @@
+import { useAppSelector } from "../../../hooks";
+import { allHolidays } from "../utils";
 
 const Holidays = () => {
+  const dob = useAppSelector((state) => state.user.dob);
+  const ctx = allHolidays();
 
-  return <div className="bg-custom-white rounded-lg shadow-lg"></div>;
+  const formatDob = () => {
+    const date = dob.split("/");
+    const year = new Date().getFullYear();
+    return `${date[0]}/${date[1]}/${year}`;
+  };
+
+  return (
+    <div className="bg-custom-white rounded-lg shadow-lg">
+      <div className="bg-indigo-800 text-custom-white px-2 py-0.5 rounded-t-lg text-sm font-medium">Paid Holidays</div>
+      <div className="p-2 grid grid-cols-2 gap-12 text-[15px] font-medium">
+        <div className="grid">
+          <div className="flex justify-between">
+            <div>New Year's Day</div>
+            <div>{ctx.newYearsDay}</div>
+          </div>
+          <div className="flex justify-between">
+            <div>Memorial Day</div>
+            <div>{ctx.memorialDay}</div>
+          </div>
+          <div className="flex justify-between">
+            <div>Independence Day</div>
+            <div>{ctx.independenceDay}</div>
+          </div>
+          <div className="flex justify-between">
+            <div>Labor Day</div>
+            <div>{ctx.laborDay}</div>
+          </div>
+        </div>
+        <div className="grid">
+          <div className="flex justify-between">
+            <div>Thanksgiving</div>
+            <div>{ctx.thanksgiving}</div>
+          </div>
+          <div className="flex justify-between">
+            <div>Black Friday</div>
+            <div>{ctx.blackFriday}</div>
+          </div>
+          <div className="flex justify-between">
+            <div>Christmas Day</div>
+            <div>{ctx.christmasDay}</div>
+          </div>
+          <div className="flex justify-between">
+            <div>Your Birthday</div>
+            <div>{formatDob()}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Holidays;
--- src/pages/timeSheet/controls/WeekEnding.tsx
+++ src/pages/timeSheet/controls/WeekEnding.tsx
@@ -17,17 +17,30 @@ const WeekEnding = () => {
   };
 
   return (
-    <div className="bg-custom-white rounded-lg shadow-indigo-200/50 shadow-md p-2">
-      <SingleSelect
-        label="Week Ending"
-        data={ctx.weekEndings}
-        displayKey="date"
-        valueKey="date"
-        onSelect={handleWESelect}
-      />
-      <div className="grid grid-cols-2 gap-2 mt-2">
-        <button className="btn-themeAmber py-1.5">Export</button>
-        <button className="btn-themeIndigo py-1.5">Email</button>
+    <div className="bg-custom-white rounded-lg shadow-indigo-200/50 shadow-md">
+      <div className="bg-indigo-800 text-custom-white px-2 py-0.5 rounded-t-lg text-sm font-medium">
+        Week Ending
+      </div>
+      <div className="p-2">
+        <SingleSelect
+          label=""
+          data={ctx.weekEndings}
+          displayKey="date"
+          valueKey="date"
+          onSelect={handleWESelect}
+        />
+        <div className="grid grid-cols-2 gap-2 mt-2">
+          <button
+            className={`btn-themeAmber py-1.5 ${ctx.rowData.length ? "" : "opacity-50 pointer-events-none"}`}
+          >
+            Export
+          </button>
+          <button
+            className={`btn-themeIndigo py-1.5 ${ctx.rowData.length ? "" : "opacity-50 pointer-events-none"}`}
+          >
+            Email
+          </button>
+        </div>
       </div>
     </div>
   );
