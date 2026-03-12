caf8c4b312994828a8f76c665913a78cd7ea4588
Stephen <stephenn649@gmail.com>
2026-03-12 11:24:48 -0500

    sets up the password reset flag form in Admin

--- src/pages/admin/forms/PasswordReset.tsx
+++ src/pages/admin/forms/PasswordReset.tsx
@@ -1,20 +1,54 @@
 import { useAdminCtx } from "../hooks";
 import { useAppDispatch } from "../../../hooks";
 import { useToast } from "../../../components/toasts/hooks/useToast";
+import { resetEmployeeInfo } from "../../../features/adminSlice";
 
 import EmployeeInfoCard from "../EmployeeInfoCard";
 import UserGrid from "../components/UserGrid";
+import { resetPWFlag } from "../../../api/admin";
 
 const PasswordReset = () => {
   const toast = useToast();
   const dispatch = useAppDispatch();
   const { employeeInfo, url, token } = useAdminCtx();
+
+  const handleClear = () => {
+    dispatch(resetEmployeeInfo());
+  };
+
+  const handleSubmit = () => {
+    resetPWFlag(url, token, employeeInfo.userid)
+      .then((resp) => {
+        const j = resp.data;
+        if (j.error === 0) {
+          handleClear();
+          toast.success("Password reset flag updated");
+        }
+      })
+      .catch((err) => toast.error(err.message));
+  };
   
   return (
     <div className="grid grid-cols-[1fr_3fr] gap-4">
       <div className="space-y-4">
         <EmployeeInfoCard />
         <UserGrid />
+      </div>
+      <div className="bg-custom-white h-[7.25rem] max-h-[7.25rem] w-[40%] rounded-lg shadow-md shadow-indigo-200/50">
+        {employeeInfo.userid ? (
+          <div className="flex flex-col justify-center items-center h-full text-center p-4">
+            <div>Reset password flag for <span className="font-medium">{employeeInfo.firstname} {employeeInfo.lastname}</span>?</div>
+            <div className="grid grid-cols-2 gap-2 mt-2 w-[77%]">
+              <button className="btn-themeAmber w-full" onClick={handleClear}>Clear</button>
+              <button className="btn-themeIndigo w-full" onClick={handleSubmit}>Reset</button>
+            </div>
+          </div>
+        ) : (
+          <div className="flex justify-center items-center h-full text-center p-4 text-sm font-medium">
+            <div>Select an employee from the left to reset their password</div>
+            <div></div>
+          </div>
+        )}
       </div>
     </div>
   );
